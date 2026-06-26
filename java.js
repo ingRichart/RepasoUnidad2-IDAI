ee0f2b90e0f017a6c48268d54d7cac0ca6b5016c
Ing. Ricardo Elizalde <ricardo.elizaldeh@un.edu.mx>
2026-06-26 09:46:04 -0600

    Avances de Java

--- java.js
+++ java.js
@@ -1,2 +1,39 @@
 
-alert("Esto se llama desde un archivo");
+/*alert("Esto se llama desde un archivo");*/
+/* nombre es del tipo de dato llamado: STRING o CADENA */
+var nombre = "Richard Elizalde";
+
+/* edad es del tipo de dato llamado: NUMERO ENTEROS */
+var edad = 46;
+
+/* altura es del tipo de dato llamado: NUMERO REAL, DECIMAL, DOUBLE */
+var altura = 1.82;
+
+/* casado es del tipo de dato llamado: BOLEANO */
+var casado = true; /* false */
+
+var num1 = 8.8;
+var num2 = 10.25;
+
+num1 = prompt("ESCRIBE EL VALOR DEL NUMERO 1:");
+num2 = prompt("ESCRIBE EL VALOR DEL NUMERO 2:");
+
+
+var result = Number(num1) + Number(num2);
+alert("El resultado de la suma de los números es: " + result);
+
+/* Estos prompt me ayudan a capturar informacion para una variable
+nombre = prompt("ESCRIBE TU NOMBRE COMPLETO:");
+edad = prompt("ESCRIBE TU EDAD:");
+altura = prompt("ESCRIBE TU ALTURA:");
+
+alert("Tu nombre es: " + nombre + ". Tu edad es de: " + edad + " y tu altura es de: " + altura);
+*/
+
+/* Estas alertas mostraron el 
+contenido de las variables
+
+alert(nombre);
+alert(edad);
+alert(altura);
+*/
