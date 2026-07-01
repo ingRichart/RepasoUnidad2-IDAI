14b6b260c0d4f4bf81f1d51702f02e2ffc6f55cf
Ing. Ricardo Elizalde <ricardo.elizaldeh@un.edu.mx>
2026-07-01 09:10:54 -0600

    Update dom.js

--- dom.js
+++ dom.js
@@ -4,18 +4,22 @@
 mensaje = function() {
     // alert("Hice click en el boton de Calcular");
 
+    //El elemento del text input
     var nom = document.getElementById("nombre");
     alert(nom);
     alert(nom.value);
 
+    //El elemento del text radio
     var marca = document.querySelector('input[name="marca"]:checked');
     alert(marca);
     alert(marca.value);
 
+    //El elemento del select con sus valores
     var dias = document.getElementById("dias");
     alert(dias);
     alert(dias.value);
 
+    //El elemento del checkbox
     var contrato = document.getElementById("contrato");
     var traslado = document.getElementById("traslado");
     var kilometraje = document.getElementById("kilometraje");
